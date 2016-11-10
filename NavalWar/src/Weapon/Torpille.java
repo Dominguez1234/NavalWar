8d33f296169b481c0e1e39ad36b990047cfb4274
Corentin Dominguez <corentin.doming@gmail.com>
2016-11-10 11:39:52 +0100

    Torpille

---
+++ NavalWar/src/Weapon/Torpille.java
@@ -0,0 +1,36 @@
+package Weapon;
+
+import BattleShip.Coord;
+
+public class Torpille extends Arme {
+	public void Torpille(Coord cible,int sens){
+		
+	}
+
+	@Override
+	public String[] needBoat() {
+		String tab[] = {"Sous-Marin"};
+		return tab;
+	}
+
+	@Override
+	public Coord[] zoneImpact(Coord cible) {
+		int x = cible.x;
+		int y = cible.y;
+		Coord coord1 = new Coord(x+1,y);
+		Coord coord2 = new Coord(x+2,y);
+		Coord coord3 = new Coord(x-1,y);
+		Coord coord4 = new Coord(x-2,y);
+		Coord coord5 = new Coord(x,y+1);
+		Coord coord6 = new Coord(x,y+2);
+		Coord coord7 = new Coord(x,y-1);
+		Coord coord8 = new Coord(x,y-2);
+		Coord coord9 = new Coord(x+1,y+1);
+		Coord coord10 = new Coord(x-1,y-1);
+		Coord coord11 = new Coord(x-1,y+1);
+		Coord coord12 = new Coord(x+1,y-1);
+		Coord tabCoord[] = {cible,coord1,coord2,coord3,coord4,coord5,coord6,coord7,coord8,coord9,coord10,coord11,coord12};
+		return tabCoord;
+	}
+
+}
