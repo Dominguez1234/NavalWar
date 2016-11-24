c5f77ff5fe7acab672c6e29f24364e9de0e873e4
Corentin <Corentin@Asidoz>
2016-11-24 08:31:41 +0100

    Correction de code

--- NavalWar/src/Weapon/Arme.java
+++ NavalWar/src/Weapon/Arme.java
@@ -9,25 +9,23 @@ import BattleShip.Coord;
 public abstract class Arme {
 	
 	public enum Sens {
-		HORIZONTAL, VERTICAL;
-
+		HORIZONTAL, 
+		VERTICAL;
 	}
 	
 	protected String name;
-	ArrayList<String> needBat = new ArrayList<String>();
+	protected ArrayList<String> needBat = new ArrayList<String>();
 	
 	public String getName() {
 		return this.name;
 	}
 
-//	public abstract String[] needBoat();
 	public abstract Coord[] zoneImpact(Coord cible, Sens sens);
 	
 	public ArrayList<String> getNeedBoat() {
 		return this.needBat;
 	}
 
-	
 	@Override
 	public String toString() {
 		return this.name+" (Arme) [needBoat()=" + this.needBat + "]";
