3cd8dc3d9debc1ee43f5c7afa8ef7bf8b9a20e06
Corentin <Corentin@10.150.1.178>
2016-12-08 09:55:10 +0100

    Ajout commentaires

--- NavalWar/src/Boats/AbstractBateau.java
+++ NavalWar/src/Boats/AbstractBateau.java
@@ -1,10 +1,16 @@
 package Boats;
 
+/*
+ * Version simplifiée d'un bateau
+ */
+
 public class AbstractBateau {
 	
+	// Attributs
 	private String nom = "";
 	private int nbrCases = 0;
 	
+	// Contruscteurs
 	public AbstractBateau(String n, int nbr) {
 		this.nom = n;
 		this.nbrCases = nbr;
