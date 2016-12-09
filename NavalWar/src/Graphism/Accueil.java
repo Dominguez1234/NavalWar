33b5dcc93994f29d85f072ab00ccdee6a2888dd2
mathieu.pierluigi <mathieu.pierluigi@L-2CE33007F6.dom1.vinci-energies.net>
2016-12-09 08:48:57 +0100

    Maj de l'accueil avec chp d'@ IP

--- NavalWar/src/Graphism/Accueil.java
+++ NavalWar/src/Graphism/Accueil.java
@@ -1,6 +1,5 @@
 package Graphism;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.EventQueue;
 
@@ -12,6 +11,8 @@ import java.awt.event.ActionEvent;
 import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
@@ -19,8 +20,8 @@ public class Accueil {
 
 	private JFrame frame;
 	private JTextField UsernameField;
-	private final JPanel panel = new JPanel(); 
-
+	private JTextField IPAddrField;
+	
 	/**
 	 * Launch the application.
 	 */
@@ -53,31 +54,7 @@ public class Accueil {
 		frame.getContentPane().setBackground(Color.WHITE);		
 		frame.getContentPane().setLayout(null);
 		
-		//Listener et bouton du mode standard
-		JButton btnModeStandard = new JButton("Mode standard");
-		btnModeStandard.setFont(new Font("Battleground", Font.PLAIN, 40));
-		btnModeStandard.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		btnModeStandard.setBounds(360, 231, 240, 80);
-		frame.getContentPane().add(btnModeStandard);
 		
-		//Listener et bouton du mode worms
-		JButton btnModeWorms = new JButton("Mode worms");
-		btnModeWorms.setFont(new Font("Battleground", Font.PLAIN, 40));
-		btnModeWorms.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		btnModeWorms.setBounds(360, 317, 240, 80);
-		frame.getContentPane().add(btnModeWorms);
-		
-		//Champ de nom
-		UsernameField = new JTextField();
-		UsernameField.setBounds(360, 186, 240, 34);
-		frame.getContentPane().add(UsernameField);
-		UsernameField.setColumns(10);
 		
 		//Label NavalWar
 		JLabel lblNavalWar = new JLabel("NavalWar", SwingConstants.CENTER);
@@ -91,25 +68,91 @@ public class Accueil {
 		lblBattleShip.setBounds(330, 50, 300, 80);		
 		frame.getContentPane().add(lblBattleShip);
 		
-		//Label du nom
-		JLabel lblUsername = new JLabel("Username:");
-		lblUsername.setFont(new Font("Tahoma", Font.PLAIN, 25));
-		lblUsername.setBounds(360, 141, 184, 34);		
-		frame.getContentPane().add(lblUsername);
-		
 		//Image de fond
 		ImageIcon image = new ImageIcon("img/Fond.png");
 		JLabel label = new JLabel("", image, JLabel.CENTER);
-		JPanel panel = new JPanel(new BorderLayout());
-		panel.add( label, BorderLayout.CENTER );
-		panel.setBounds(0, 0, 960, 540);
+		label.setBounds(0, 0, 960, 511);
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+		
+		final JButton btnConnect = new JButton("Connect");
+		btnConnect.setFont(new Font("Battleground", Font.PLAIN, 30));
+		btnConnect.setBounds(625, 181, 150, 34);
+		panel.add(btnConnect);
+		
+		//Listener et bouton du mode standard
+		JButton btnModeStandard = new JButton("Mode standard");
+		btnModeStandard.setBounds(360, 310, 240, 80);
+		panel.add(btnModeStandard);
+		btnModeStandard.setFont(new Font("Battleground", Font.PLAIN, 40));
+		btnModeStandard.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		
+		JLabel lblIPAddr = new JLabel("IP Address:");
+		lblIPAddr.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		lblIPAddr.setBounds(360, 136, 184, 34);
+		panel.add(lblIPAddr);
+		
+		//Label du nom
+		JLabel lblUsername = new JLabel("Username:");
+		lblUsername.setBounds(360, 220, 184, 34);
+		panel.add(lblUsername);
+		lblUsername.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		
+		//Listener et bouton du mode worms
+		JButton btnModeWorms = new JButton("Mode worms");
+		btnModeWorms.setBounds(360, 396, 240, 80);
+		panel.add(btnModeWorms);
+		btnModeWorms.setFont(new Font("Battleground", Font.PLAIN, 40));
+		btnModeWorms.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		panel.add( label );
+		panel.setBounds(0, 0, 960, 511);
 		
 		
 		frame.getContentPane().add(panel);
+		
+		//Champ de nom
+		UsernameField = new JTextField();
+		UsernameField.setBounds(360, 265, 240, 34);
+		panel.add(UsernameField);
+		UsernameField.setColumns(10);
+		
+		IPAddrField = new JTextField();
+		IPAddrField.setColumns(10);
+		IPAddrField.setBounds(360, 181, 240, 34);
+		panel.add(IPAddrField);
 		frame.setBounds(100, 100, 450, 300);
 		frame.setBackground(new Color(0, 153, 204));
 		frame.setSize(960,540);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+				
+		IPAddrField.getDocument().addDocumentListener(new DocumentListener() {
+			  public void changedUpdate(DocumentEvent e) {
+			    changed();
+			  }
+			  public void removeUpdate(DocumentEvent e) {
+			    changed();
+			  }
+			  public void insertUpdate(DocumentEvent e) {
+			    changed();
+			  }
+
+			  public void changed() {
+			     if (IPAddrField.getText().equals("")){
+			    	 btnConnect.setEnabled(false);
+			     }
+			     else {
+			    	 btnConnect.setEnabled(true);
+			    }
+
+			  }
+			});
 		
 		
 	}
