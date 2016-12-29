8a2fc500bd02a84473beec1f7ba7a1a4b58be83b
A54876 <A54876@DSP0681271.atlas.edf.fr>
2016-12-29 15:06:06 +0100

    test du jeu en mode console => problème réseau

--- NavalWar/src/BattleShip/Test.java
+++ NavalWar/src/BattleShip/Test.java
@@ -12,88 +12,9 @@ import Weapon.*;
 public class Test {
 	
 	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
-
-//		Coord pos1 = new Coord(0,0);
-//		Coord pos2 = new Coord(1,1);
-//		Coord pos3 = new Coord(5,4);
-//		Coord pos4 = new Coord(7,2);
-//		Coord pos5 = new Coord(1,8);
-//		
-//		Ocean oc = new Ocean(Ocean.modeJeu.TOTALWAR);
-//		oc.monJeu.setPosition("Porte-Avions", pos1, Bateau.direction.verticale);
-//		oc.monJeu.setPosition("Croiseur", pos2, Bateau.direction.horizontale);
-//		oc.monJeu.setPosition("Sous-Marin", pos3, Bateau.direction.horizontale);
-//		oc.monJeu.setPosition("Destroyer", pos4, Bateau.direction.verticale);
-//		oc.monJeu.setPosition("Patrouilleur", pos5, Bateau.direction.verticale);
-		
-// --------------------------------------------------------------------------------------------
-		
-//		Torpille m = new Torpille();
-//
-//		System.out.println(m);
-//		System.out.println(m.zoneImpact(new Coord(1,1), Arme.Sens.VERTICAL));
-//
-//		
-//		Scanner sc = new Scanner(System.in);
-//		Coord cible = new Coord(-1,-1);
-//		String arme = "Torpille";
-//		while(true) {
-//			System.out.println(oc);
-//			
-//			System.out.print("Ligne : ");
-//			cible.x = sc.nextInt()-1;
-//			System.out.print("Colonne : ");
-//			cible.y = sc.nextInt()-1;
-//			
-//			System.out.println(oc.fire(cible,arme,Arme.Sens.HORIZONTAL));
-//			
-//		}
-		
-// --------------------------------------------------------------------------------------------
-// ----- Test du réseau
-		
-//		Reseau r = new Reseau(3339,"127.0.0.1");
-//		Tir tir = new Tir(new Coord(1,1),"Missile");
-//		r.send(tir);
-//		
-//		r.receive();
-		
-// --------------------------------------------------------------------------------------------
-		
-		// Connexion à l'autre
-//		String ip = "127.0.0.1";
-//		int port = 3339;
-//		
-//		Reseau r = new Reseau(ip,port);
-//		r.connexion();
-//		
-//		try {
-//			Thread.sleep(1000);
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		
-//		System.out.println("Missile lancé !");
-//		Tir t = new Tir();
-//		t.nomArme = "Missile";
-//		t.addCible(new Coord(1,2));
-//		r.send(t);
-//		
-//		try {
-//			Thread.sleep(1000);
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		
-//		t = r.receive();
-//		System.out.println(t);
-//		
-// --------------------------------------------------------------------------------------------
 		
 		BattleShip bs = new BattleShip(BattleShip.modeJeu.TOTALWAR);
-		bs.connexion("127.0.0.1", 3131);
+		bs.connexion("127.0.0.1");
 		
 		bs.setPosBoat("Porte-Avions", new Coord(0,0), Bateau.direction.verticale);
 		bs.setPosBoat("Croiseur", new Coord(1,1), Bateau.direction.horizontale);
@@ -102,20 +23,25 @@ public class Test {
 		bs.setPosBoat("Patrouilleur", new Coord(1,8), Bateau.direction.verticale);
 		
 		
-		System.out.println("Test prêt\n");
+		System.out.println("Test pr�t\n");
 		
 		Thread.sleep(2000);
 		
-		Coord cible = new Coord();
-		Scanner sc = new Scanner(System.in);
-		System.out.print("Ligne : ");
-		cible.x = sc.nextInt()-1;
-		System.out.print("Colonne : ");
-		cible.y = sc.nextInt()-1;
+		while(true) {
+			Coord cible = new Coord();
+			Scanner sc = new Scanner(System.in);
+			System.out.print("Ligne : ");
+			cible.x = sc.nextInt()-1;
+			System.out.print("Colonne : ");
+			cible.y = sc.nextInt()-1;
+			
+			bs.jAttaque(cible, "Nuke", Arme.Sens.HORIZONTAL);
+			System.out.println(bs.ocean);
+			
+			bs.jAttendsLattaque();
+		}
 		
-		bs.jAttaque(cible, "Nuke", Arme.Sens.HORIZONTAL);
 		
-//		System.out.println(bs.ocean);
 		
 	}
 	
--- NavalWar/src/BattleShip/Test2.java
+++ NavalWar/src/BattleShip/Test2.java
@@ -1,83 +1,15 @@
 package BattleShip;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 import Boats.Bateau;
+import Weapon.Arme;
 
 public class Test2 {
 	
 	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
-
-//		Coord pos1 = new Coord(1,1);
-//		Coord pos2 = new Coord(3,0);
-//		Coord pos3 = new Coord(4,5);
-//		Coord pos4 = new Coord(2,9);
-//		Coord pos5 = new Coord(7,6);
-//		
-//		Ocean oc = new Ocean(Ocean.modeJeu.TOTALWAR);
-//		oc.monJeu.setPosition("Porte-Avions", pos1, Bateau.direction.horizontale);
-//		oc.monJeu.setPosition("Croiseur", pos2, Bateau.direction.verticale);
-//		oc.monJeu.setPosition("Sous-Marin", pos3, Bateau.direction.horizontale);
-//		oc.monJeu.setPosition("Destroyer", pos4, Bateau.direction.verticale);
-//		oc.monJeu.setPosition("Patrouilleur", pos5, Bateau.direction.verticale);
 		
-// --------------------------------------------------------------------------------------------
-		
-//		Torpille m = new Torpille();
-//
-//		System.out.println(m);
-//		System.out.println(m.zoneImpact(new Coord(1,1), Arme.Sens.VERTICAL));
-//
-//		
-//		Scanner sc = new Scanner(System.in);
-//		Coord cible = new Coord(-1,-1);
-//		String arme = "Torpille";
-//		while(true) {
-//			System.out.println(oc);
-//			
-//			System.out.print("Ligne : ");
-//			cible.x = sc.nextInt()-1;
-//			System.out.print("Colonne : ");
-//			cible.y = sc.nextInt()-1;
-//			
-//			System.out.println(oc.fire(cible,arme,Arme.Sens.HORIZONTAL));
-//			
-//		}
-		
-// --------------------------------------------------------------------------------------------
-// ----- Test du réseau
-		
-//		Reseau r = new Reseau(3339,"127.0.0.1");
-//		Tir tir = new Tir(new Coord(1,1),"Missile");
-//		r.send(tir);
-		
-//		Tir tir = r.receive();
-		
-// --------------------------------------------------------------------------------------------
-		
-		// Connexion à l'autre
-//		String ip = "127.0.0.1";
-//		int port = 3339;
-//		
-//		Reseau r = new Reseau(ip,port);
-//		r.connexion();
-//		
-//		Tir t = r.receive();
-//		System.out.println("Nous sommes touchés !");
-//		t.addTouche(new Coord(1,2));
-//		
-//		try {
-//			Thread.sleep(1000);
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		
-//		r.send(t);
-		
-// --------------------------------------------------------------------------------------------
-		
-		while(true) {
 			BattleShip bs = new BattleShip(BattleShip.modeJeu.TOTALWAR);
 			bs.connexion("127.0.0.1");
 			
@@ -90,8 +22,22 @@ public class Test2 {
 			System.out.println("Test2 prêt\n");
 			
 			Thread.sleep(1000);
-			bs.jAttendsLattaque();
-		}
+			
+			while(true) {
+				bs.jAttendsLattaque();
+				
+				Coord cible = new Coord();
+				Scanner sc = new Scanner(System.in);
+				System.out.print("Ligne : ");
+				cible.x = sc.nextInt()-1;
+				System.out.print("Colonne : ");
+				cible.y = sc.nextInt()-1;
+				
+				bs.jAttaque(cible, "Nuke", Arme.Sens.HORIZONTAL);
+				System.out.println(bs.ocean);
+				
+				
+			}
 		
 		
 	}
