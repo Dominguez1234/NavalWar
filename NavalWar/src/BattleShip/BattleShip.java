482bee26661074c22759edb670bfcd269d7a65df
Corentin <Corentin@192.168.1.10>
2016-12-31 12:32:48 +0100

    Changement des timers entre attaque et attente

--- NavalWar/src/BattleShip/BattleShip.java
+++ NavalWar/src/BattleShip/BattleShip.java
@@ -108,7 +108,7 @@ public class BattleShip {
 			
 				// --- A SUPPRIMER
 				try {
-					Thread.sleep(1000);
+					Thread.sleep(100);
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
@@ -165,7 +165,7 @@ public class BattleShip {
 		
 		// --- A SUPPRIMER
 		try {
-			Thread.sleep(2000);
+			Thread.sleep(200);
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
